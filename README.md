ca1ae16fe9479119304e8c0cc1e0e06bcb61b8a1
Kathan <sonikathan2001@gmail.com>
2022-08-09 00:30:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## About The Project
+
+Created House price prediction on the 2015-2020 house data of California city.
+
---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pickle
+model= loaded_model = pickle.load(open("noscaled_house_prize_simple_lin_reg.pkl", 'rb'))
+st.title("house price prediction")
+
+def predict_price(tra_date,house_age,dst_mrt,con_stores,latitude,longitude):
+
+
+    prediction=model.predict([[tra_date,house_age,dst_mrt,con_stores,latitude,longitude]])
+    return  prediction
+
+def main():
+
+    tra_date=st.number_input("transaction date")
+    house_age=st.number_input("House age")
+    dst_mrt=st.number_input("distance to the nearest MRT station")
+    con_stores=st.number_input("number of convenience stores")
+    latitude=st.number_input("latitude")
+    longitude=st.number_input("longitude")
+    result=""
+    btn=st.button("predict")
+    if btn:
+        result=predict_price(tra_date,house_age,dst_mrt,con_stores,latitude,longitude)
+    st.success(f"The output is : {result} ")
+
+if __name__ == '__main__':
+    main()
